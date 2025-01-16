b8126c62d14dd18e0dba1904cf8641c9391bc6d8
André <49207467+andrem81@users.noreply.github.com>
2025-01-16 17:24:54 -0300

    Update desafio de python sistema de banco.py

--- desafio de python sistema de banco.py
+++ desafio de python sistema de banco.py
@@ -1,5 +1,5 @@
 def menu():
-    menu = """ \n
+    menu_text = """ 
                 Menu
     ============================
     [d] Depositar
@@ -8,53 +8,36 @@ def menu():
     [q] Sair
     ============================
     => """
-    return input(menu)
- 
-def func_deposito(saldo, valor, extrato, /):
+    return input(menu_text)
 
-        if valor > 0:
-            saldo += valor
-            extrato += f"Depósito: R$ {valor}\n"
+def deposit(saldo, valor, extrato):
+    if valor > 0:
+        saldo += valor
+        extrato += f"Depósito: R$ {valor}\n"
+    else:
+        print("Operação falhou! O valor informado é inválido.")
+    return saldo, extrato
 
-        else:
-            print("Operação falhou! O valor informado é inválido.")
-        return saldo, extrato
-
-def func_saque(*, saldo, valor, extrato, limite, numero_saques, limite_saques):
-    valor = float(input("Informe quanto deseja sacar: "))
-
-    sem_limite = valor > limite
-
-    sem_saques = numero_saques >= LIMITE_SAQUES
-        
-    sem_saldo = valor > saldo
-    
-    if sem_limite:
-        print("ERRoR!O valor excede o limite máximo.")
-
-    elif sem_saques:
-        print("ERRoR! Você excedeu o número máximo de saques.")
-
-    elif sem_saldo:
-        print("ERRoR! Você não possui saldo suficiente.")
-
+def withdraw(saldo, valor, extrato, limite, numero_saques, limite_saques):
+    if valor > limite:
+        print("Error! O valor excede o limite máximo.")
+    elif numero_saques >= limite_saques:
+        print("Error! Você excedeu o número máximo de saques.")
+    elif valor > saldo:
+        print("Error! Você não possui saldo suficiente.")
     elif valor > 0:
         saldo -= valor
         extrato += f"Saque: R$ {valor:.2f}\n"
         numero_saques += 1
-
     else:
-        print("ERRoR! O valor informado é inválido, digite um valor válido.")
+        print("Error! O valor informado é inválido, digite um valor válido.")
     return saldo, extrato, numero_saques
 
-def func_extrato(saldo, /, *, extrato):
+def print_statement(saldo, extrato):
     print("=========== EXTRATO BANCÁRIO =============")
-    print("Nenhuma movimentação realizxada." if not extrato else extrato)
+    print("Nenhuma movimentação realizada." if not extrato else extrato)
     print(f"Seu saldo é de: R$ {saldo:.2f}")
-    print("==================================")
-
-
-
+    print("==========================================")
 
 saldo = 0
 limite = 500
@@ -65,20 +48,21 @@ LIMITE_SAQUES = 3
 while True:
     opcao = menu()
     
-
     if opcao == "d":
         valor = float(input("Informe o valor do depósito: "))
-        saldo, extrato = func_deposito(saldo, valor, extrato);
+        saldo, extrato = deposit(saldo, valor, extrato)
     elif opcao == "s":
-        saldo, extrato, numero_saques = func_saque(
-                saldo=saldo,
-                valor=valor,
-                extrato=extrato,
-                limite=limite,
-                numero_saques=numero_saques,
-                limite_saques=LIMITE_SAQUES,)
+        valor = float(input("Informe quanto deseja sacar: "))
+        saldo, extrato, numero_saques = withdraw(
+            saldo=saldo,
+            valor=valor,
+            extrato=extrato,
+            limite=limite,
+            numero_saques=numero_saques,
+            limite_saques=LIMITE_SAQUES
+        )
     elif opcao == "e":
-        func_extrato(saldo, extrato=extrato);
+        print_statement(saldo, extrato)
     elif opcao == "q":
         break
     else:
